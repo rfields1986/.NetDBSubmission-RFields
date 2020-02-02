14b6c778413546f650b3ed3fed1ab4d8dbabffe1
Ryan Fields <rfields@my.wctc.edu>
2020-02-02 13:30:38 -0600

    Case 2: Was written

--- Week 2 Homework/Program.cs
+++ Week 2 Homework/Program.cs
@@ -13,7 +13,7 @@ namespace Week_2_Homework
         {
             int exit = 0, input;
             string file = "Tickets.csv";
-            string tick, sum, stat, prior, sub, ass, watch;
+            
             do
             {
                 //ask for choice
@@ -30,15 +30,32 @@ namespace Week_2_Homework
                             string[] arr = line1.Split(',');
                             Console.WriteLine($"Ticket ID: {arr[0]}\nSummary: {arr[1]}\nStatus: {arr[2]}\nPriority: {arr[3]}\n" +
                                 $"Submitter: {arr[4]}\nAssigned: {arr[5]}\nWatching: {arr[6]}\n");
-                            rd.Close();
+                            
                             
                         }
+                        rd.Close();
                         break;
 
                     case 2:
                         {
                             StreamWriter rd1 = new StreamWriter(file, append: true);
-
+                            string ticketID, summary, status, priority, submitter, assigned, watching;
+                            Console.WriteLine("Please enter your ticket number?");
+                            ticketID = Console.ReadLine();
+                            Console.WriteLine("Please enter a summary of the issue?");
+                            summary = Console.ReadLine();
+                            Console.WriteLine("What the status of this ticket?");
+                            status = Console.ReadLine();
+                            Console.WriteLine("Please enter a priority: High, Medium, or Low?");
+                            priority = Console.ReadLine();
+                            Console.WriteLine("What is your name?");
+                            submitter = Console.ReadLine();
+                            Console.WriteLine("Who is the ticket assigned too?");
+                            assigned = Console.ReadLine();
+                            Console.WriteLine("Who will be watching this ticket?");
+                            watching = Console.ReadLine();
+                            rd1.WriteLine($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{watching}");
+                            rd1.Close();
 
                         }
                         break;
