508cd6c80865f7bf6e970570c3f2591049135ebf
Ryan Fields <rfields@my.wctc.edu>
2020-02-16 19:00:48 -0600

    Complete Week 4 Assignment

--- Week 2 Homework/Program.cs
+++ Week 2 Homework/Program.cs
@@ -12,7 +12,7 @@ namespace Week_2_Homework
         static void Main(string[] args)
         {
             int exit = 0, input;
-            string file = "Tickets.csv";
+
             
             do
             {
@@ -25,60 +25,15 @@ namespace Week_2_Homework
                 {
                     case 1:
                         exit = 1;
-                        StreamReader rd = new StreamReader(file);
-                        string line = rd.ReadLine();
-                        string[] header = line.Split(',');
-                        while (!rd.EndOfStream)
-                        {
-                            string line1 = rd.ReadLine();
-                            string[] body = line1.Split(',');
-                            for (int i = 0; i < body.Length; i++)
-                            {
-                                Console.Write("{0,-20}",header[i]) ;
-                                Console.WriteLine(body[i]);
+                        ReadTicketFile();
 
-                            }
-                            Console.WriteLine("\n");
-
-
-
-
-                        }
-                        Console.WriteLine("Press Enter To Return To The Main Menu");
-                        Console.ReadKey();
-                        rd.Close();
                         break;
 
                     case 2:
                         {
                             exit = 1;
-                            StreamWriter rd1 = new StreamWriter(file, append: true);
-                            string ticketID, summary, status, priority, submitter, assigned, watching;
-                            Console.WriteLine("Please enter your ticket number?");
-                            ticketID = Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("Please enter a summary of the issue?");
-                            summary = Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("What the status of this ticket?");
-                            status = Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("Please enter a priority: High, Medium, or Low?");
-                            priority = Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("What is your name?");
-                            submitter = Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("Who is the ticket assigned too?");
-                            assigned = Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("Who will be watching this ticket? Seperate multiple watchers with \"|\"");
-                            watching = Console.ReadLine();
-                            Console.Clear();
-                            rd1.WriteLine($"{ticketID},{summary},{status},{priority},{submitter},{assigned},{watching}");
-                            rd1.Close();
-                            Console.WriteLine("Press Enter To Return To The Main Menu");
-                            Console.ReadKey();
+                            Ticket ticketEntry = new Ticket();
+                            ticketEntry.CreateNewTicket();
 
                         }
                         break;
@@ -91,5 +46,32 @@ namespace Week_2_Homework
                 }
             } while (exit != 0);
         }
+        public static void ReadTicketFile()
+        {
+            string file = "Tickets.csv";
+            StreamReader rd = new StreamReader(file);
+            string line = rd.ReadLine();
+            string[] header = line.Split(',');
+            while (!rd.EndOfStream)
+            {
+                string line1 = rd.ReadLine();
+                string[] body = line1.Split(',');
+                for (int i = 0; i < body.Length; i++)
+                {
+                    Console.Write("{0,-20}", header[i]);
+                    Console.WriteLine(body[i]);
+
+                }
+                Console.WriteLine("\n");
+
+
+
+
+            }
+            Console.WriteLine("Press Enter To Return To The Main Menu");
+            Console.ReadKey();
+            rd.Close();
+        }
+
     }
 }
